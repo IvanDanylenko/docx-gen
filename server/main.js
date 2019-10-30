7b5984a549ff09f227d13aa49b49bec46dde05b1
Ivan Danylenko <danylenko.ivan11@gmail.com>
2019-10-30 12:34:45 +0200

    install officegen, playing with settings

--- server/main.js
+++ server/main.js
@@ -1,5 +1,53 @@
-const app = require('./app/server');
+// const app = require('./app/server');
 
-app.listen(8080, function(){
-  console.log('you may use rest api at port 8080');
-});
+// app.listen(8080, function(){
+//   console.log('you may use rest api at port 8080');
+// });
+
+
+/* OFFICE GEN TEST */
+const officegen = require('officegen')
+const fs = require('fs')
+
+// Create an empty Word object:
+let docx = officegen({
+  type: 'docx',
+  pageMargins: {
+    top: 1600,
+    right: 1440,
+    bottom: 1800,
+    left: 1600
+  }
+})
+
+// Officegen calling this function after finishing to generate the docx document:
+docx.on('finalize', function(written) {
+  console.log(
+    'Finish to create a Microsoft Word document.'
+  )
+})
+
+// Officegen calling this function to report errors:
+docx.on('error', function(err) {
+  console.log(err)
+})
+
+// Create a new paragraph:
+let pObj = docx.createP()
+
+pObj.addText('Документ згенеровано', { highlight: true }) // Highlight!
+
+pObj = docx.createP()
+
+pObj = docx.createP()
+
+// Let's generate the Word document into a file:
+
+let out = fs.createWriteStream('example.docx')
+
+out.on('error', function(err) {
+  console.log(err)
+})
+
+// Async call to generate the output file:
+docx.generate(out)
