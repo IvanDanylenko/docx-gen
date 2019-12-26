af5df5162305f6f13298acd0b0c6dd4c49da499e
Ivan Danylenko <danylenko.ivan11@gmail.com>
2019-12-26 10:01:10 +0200

    new beginning

--- server/app/controllers/vidomostiApi.js
+++ server/app/controllers/vidomostiApi.js
@@ -1,3 +1,17 @@
+const createReport = require('docx-templates');
+
+function writeToDocTemplateFile(data) {
+ 
+  // createReport({
+  //   template: '/app/templates/docx-templates/vidomist.docx',
+  //   // output: 'a/templates/docx-templates/output/vidomist-output.docx',
+  //   data: {
+  //     name: 'John',
+  //     surname: 'Appleseed',
+  //   }
+  // });
+}
+
 exports.generateVidomosti = function (req, res) {
 
   /* Успешно читает xlsx файлы */
@@ -13,22 +27,43 @@ exports.generateVidomosti = function (req, res) {
   //     console.log(e)
   //   })
 
-  const data = require('../templates/data.json');
-  let output;
 
-  for (let item in data) {
-    const t = item.indexOf('Підрозділ');
-    if (t !== -1) {
-      output = t;
-      break;
-    }
+  // const data = require('../templates/data.json');
+  // let output;
+
+  // for (let item in data) {
+  //   const t = item.indexOf('Підрозділ');
+  //   if (t !== -1) {
+  //     output = t;
+  //     break;
+  //   }
+  // }
+
+  const mockData = {
+    
   }
 
+// Idea to parse array
   // const result = Object.values(yourJSON.roots)
   //   .map(flatten)
   //   .reduce((a,b) => a.concat(b), []);
   
-  res.json({data: output});
+  /* res.json({data: output}); */
+  try {
+    createReport({
+      template: './app/templates/docx-templates/vidomist2.doc',
+      // output: '../templates/docx-templates/output/vidomist-output.docx',
+      data: {
+        name: 'John',
+        surname: 'Appleseed',
+      }
+    });
+  }
+  catch(error) {
+    console.log(error)
+  }
+  
+  res.json({data: "generated"});
 
   
   // res.status(200).json({ data: 'Vidomosty generated' });
